963b26c38ed8c000e6e2e41cdc51d7d879e6bfcf
majidpawar9 <majidpawar9@gmail.com>
2023-08-07 19:52:42 -0400

    added edit todo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -105,5 +105,4 @@ public class HomeController : Controller
 
         return RedirectToAction("Index");
     }
-
 }
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -5,6 +5,7 @@ public class Todo
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public string Due {get; set; }
     public bool IsDone { get; set; }
 }
 }
