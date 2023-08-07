2eb46fb0ff82f25fd63f7b6d429679737b31b1f7
majidpawar9 <majidpawar9@gmail.com>
2023-08-07 16:40:45 -0400

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web_1001_ToDoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
